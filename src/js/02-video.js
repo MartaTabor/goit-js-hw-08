6d9620f9159a23bcf93b5e30da3c159760c23e20
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-30 21:05:54 +0000

    task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,8 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-const currentTime = throttle(timeupdate => {
-  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
+const currentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }, 1000);
 
 player.on('timeupdate', currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector("input[name='email']");
+const message = form.querySelector("textarea[name='message']");
+
+const saveToStorage = () => {
+  const state = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+};
+
+const loadFromStorage = () => {
+  const stored = localStorage.getItem('feedback-form-state');
+  if (stored) {
+    const state = JSON.parse(stored);
+    email.value = state.email;
+    message.value = state.message;
+  }
+};
+
+const submitAction = event => {
+  event.preventDefault();
+  console.log('Form Data:', {
+    email: email.value,
+    message: message.value,
+  });
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+};
+
+form.addEventListener('input', throttle(saveToStorage), 500);
+form.addEventListener('change', saveToStorage);
+form.addEventListener('submit', submitAction);
+loadFromStorage();
