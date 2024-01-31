dfbf33820bf3c809ea77a31335f1c72fb093b6a0
Marta Tabor <leeloothefifth@googlemail.com>
2024-01-31 20:38:25 +0000

    task-03-update2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,35 +4,35 @@ const form = document.querySelector('.feedback-form');
 const email = form.querySelector("input[name='email']");
 const message = form.querySelector("textarea[name='message']");
 
-const saveToStorage = () => {
+const storageKey = 'feedback-form-state';
+
+const saveToStorage = throttle(() => {
   const state = {
     email: email.value.trim(),
     message: message.value.trim(),
   };
-  localStorage.setItem('feedback-form-state', JSON.stringify(state));
-};
+  localStorage.setItem(storageKey, JSON.stringify(state));
+}, 500);
+
+form.addEventListener('input', saveToStorage);
 
 const loadFromStorage = () => {
-  const stored = localStorage.getItem('feedback-form-state');
+  const stored = localStorage.getItem(storageKey);
   if (stored) {
-    const state = JSON.parse(stored);
-    email.value = state.email;
-    message.value = state.message;
+    try {
+      const state = JSON.parse(stored);
+      email.value = state.email;
+      message.value = state.message;
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 };
 
-const submitAction = event => {
-  event.preventDefault();
-  console.log('Form Data:', {
-    email: email.value,
-    message: message.value,
-  });
-  localStorage.removeItem('feedback-form-state');
-  email.value = '';
-  message.value = '';
-};
-
-form.addEventListener('input', throttle(saveToStorage), 500);
-form.addEventListener('change', saveToStorage);
-form.addEventListener('submit', submitAction);
 loadFromStorage();
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem(storageKey);
+  form.reset();
+});
